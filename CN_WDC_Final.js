56b8573471c1e50199cf9c7c5917d54e88fb31f4
suraj-converse <71890617+suraj-converse@users.noreply.github.com>
2020-10-09 14:30:17 +0530

    Update CN_WDC_Final.js

--- CN_WDC_Final.js
+++ CN_WDC_Final.js
@@ -1,22 +1,15 @@
 (function() {
     // Create the connector object
     var myConnector = tableau.makeConnector();
-
+​
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
 		// Define Columns
         var cols = [{
-            id: "uuid",
-			alis:"UUID",
+            id: "personalization_key",
             dataType: tableau.dataTypeEnum.string
         },{
             id: "user_name",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "personalization_key",
-            dataType: tableau.dataTypeEnum.string
-        } ,{
-            id: "status",
             dataType: tableau.dataTypeEnum.string
         },{
             id: "start_time",
@@ -29,37 +22,49 @@
             alias: "Ordered Dishes",
             dataType: tableau.dataTypeEnum.string
         },{
-            id: "is_pushed",
+            id: "upsell",
+            alias: "Upsell",
             dataType: tableau.dataTypeEnum.bool
         },{
             id: "items_number",
             dataType: tableau.dataTypeEnum.int
         },{
-            id: "feedback",
-            dataType: tableau.dataTypeEnum.string
-        },        
-        {
-            id: "coupons_applied",
+            id: "net_price",
             dataType: tableau.dataTypeEnum.float
+        },{
+            id: "is_pushed",
+            dataType: tableau.dataTypeEnum.bool
+        },{
+            id: "ai_assisted",
+            dataType: tableau.dataTypeEnum.bool
         }];
-
+​
         var tableSchema = {
             id: "DriveThru",
             alias: "Converse Now DriveThru Data",
             columns: cols
         };
-
+​
         schemaCallback([tableSchema]);
     };
-
+​
     // Download the data
     myConnector.getData = function(table, doneCallback) {
         $.getJSON("https://radagast.staging.v2.conversenow.ai/v1/metrics/trackers?from=2020-08-01&to=2020-08-08", function(resp) {
             var feat = resp.trackers,
                 tableData = [];
-
+​
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
+                var dishlist = feat[i].dishes;
+                var items = ""
+                for (var j = 0, len1 = dishlist.length; j < len1; j++) {
+                    if (items != ""){
+                        items = items + ", " + dishlist[j]
+                    } else {
+                        items = dishlist[j]
+                    }
+                }
                 tableData.push({
                     "uuid": feat[i].uuid,
                     "user_name": feat[i].user_name,
@@ -68,27 +73,23 @@
 					"personalization_key":feat[i].personalization_key,
 					"start_time":feat[i].start_time,
 					"end_time":feat[i].end_time,
+					"dishes":items,
+                    "net_price":feat[i].sales,
 					"is_pushed":feat[i].is_pushed,
-					"item numbers":feat[i].items_numbers,
 					"coupons_applied":feat[i].coupons_applied,
-					"feedback":feat[i].feedback
+					"feedback":feat[i].feedback,
+                    "upsell":feat[i].upsell,
+                    "ai_assisted":feat[i].ai_assisted
                 });
             }
-		var feats = resp.trackers.dishes;
-	    for (var j = 0, len = feats.length; j < len; j++) {
-                tableData.push({
-   			"dishes":feats[j]
-                });
-            }
-	
-
+​
             table.appendRows(tableData);
             doneCallback();
         });
     };
-
+​
     tableau.registerConnector(myConnector);
-
+​
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
